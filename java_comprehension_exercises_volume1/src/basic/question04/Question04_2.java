86479162b43bfc0aa38d384beba0be561e7c1ec9
adachijo <adachijo@C010422893.zipangu.cac.co.jp>
2026-04-16 13:59:38 +0900

    完了しました。

--- java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
+++ java_comprehension_exercises_volume1/src/basic/question04/Question04_2.java
@@ -1,6 +1,8 @@
 package basic.question04;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 第7章 配列
@@ -9,9 +11,19 @@ import java.io.IOException;
 public class Question04_2 {
 
 	public static void main(String[] args) throws IOException {
-
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		// TODO:ここに実装
-		
+		int[] ageGroup = { 23, 33, 26, 21, 25, 22 };
+
+		System.out.println("４番目の人の年齢を入力してください。");
+		String str = reader.readLine();
+		int temporaryAge = Integer.parseInt(str);
+
+		ageGroup[4] = temporaryAge;
+
+		System.out.println("４番目の人の年齢は" + ageGroup[4] + "歳です。");
+		System.out.println("人数は" + ageGroup.length + "人です。");
+
 	}
 
 }
